26ab736f3f8a46eb6edbbfe442fef679966ed147
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-09-09 16:59:04 +0300

    changes to Lab1

--- Lab1/lab1.rb
+++ Lab1/lab1.rb
@@ -1,3 +1,16 @@
+def s(a, b, c, x, phi)
+  numerator = Math.sin(3*x)**3 + Math.atan(phi) - 6 * (10 ** 3.1)
+  denominator = Math.sqrt(a*x**2 + b*x + c)
+  second_addend = Math.exp(x) * Math.tan(x+2).abs
+
+  # check if denominator = 0 (with some precision), if so notify the user, we still can print the result: -Infinity
+  if denominator.zero?
+    puts "Devision by zero!"
+  end
+
+  numerator / denominator + second_addend
+end
+
 # obtaining input
 print "Please, enter a ="
 a = gets.chomp.to_f
@@ -16,16 +29,7 @@ phi = gets.chomp.to_f
 
 
 # calculating the result
-numerator = Math.sin(3*x)**3 + Math.atan(phi) - 6 * (10 ** 3.1)
-denominator = Math.sqrt(a*x**2 + b*x + c)
-second_addend = Math.exp(x) * Math.tan(x+2).abs
-
-# check if denominator = 0 (with some precision), if so notify the user, we still can print the result: -Infinity
-if denominator.zero?
-  puts "Devision by zero!"
-end
-
-result = numerator / denominator + second_addend
+result = s(a, b, c, x, phi)
 
 
 # printing the result
