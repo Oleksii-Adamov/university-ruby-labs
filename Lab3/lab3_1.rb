a84911f74da008e3353509341d38346adc89e7fa
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-09-23 19:39:49 +0300

    Added unit tests to Lab3 1,2

--- Lab3/lab3_1.rb
+++ Lab3/lab3_1.rb
@@ -1,28 +1,86 @@
+require 'minitest/autorun'
+
 # 1)
+def func_a(a, b)
+  !(a || b) && (a && !b)
+end
 
-# input
-a = true
-b = false
-c = true
-x = 2
-y = 10
-z = -50
+def func_b(a, b, c, x, y, z)
+  (z != y).object_id <= (6 >= y).object_id && a || b && c && x >= 1.5
+end
 
-# computing and printing answers
-puts "1)"
-puts "  a) #{!(a || b) && (a && !b)}"
-puts "  b) #{(z != y).object_id <= (6 >= y).object_id && a || b && c && x >= 1.5}"
-puts "  c) #{(8 - x*2 <= z) && (x**2 <= y**2) || (z >= 15)}"
-puts "  d) #{x > 0 && y < 0 || z >= (x*y-y/x) + (-z)}"
-puts "  e) #{!(a || b && !(c || (!a || b)))}"
-puts "  f) #{x**2 + y**2 >= 1 && x >= 0 && y >= 0}"
-puts "  g) #{(a && (c && b != b || a) || c) && b}"
+def func_c(x, y, z)
+  (8 - x*2 <= z) && (x**2 <= y**2) || (z >= 15)
+end
+
+def func_d(x, y, z)
+  x > 0 && y < 0 || z >= (x*y-y/x) + (-z)
+end
+
+def func_e(a, b, c)
+  !(a || b && !(c || (!a || b)))
+end
+
+def func_f(x, y, z)
+  x**2 + y**2 >= 1 && x >= 0 && y >= 0
+end
+
+def func_g(a, b, c)
+  (a && (c && b != b || a) || c) && b
+end
 
 # 2)
+def func_second(a, p)
+  ((Math.log(a) > a) || !p) && (Math.sqrt(a) < a**4)
+end
 
-# input
-a = 2.5
-p = true
+#Tests
+class UnitTest1 < MiniTest::Unit::TestCase
+  def setup
+    @a = true
+    @b = false
+    @c = true
+    @x = 2
+    @y = 10
+    @z = -50
+  end
 
-# computing and printing answers
-puts "2) #{((Math.log(a) > a) || !p) && (Math.sqrt(a) < a**4)}"
+  def test_a
+    assert_equal(false, func_a(@a, @b))
+  end
+
+  def test_b
+    assert_equal(false, func_b(@a, @b, @c, @x, @y, @z))
+  end
+
+  def test_c
+    assert_equal(false, func_c(@x, @y, @z))
+  end
+
+  def test_d
+    assert_equal(false, func_d(@x, @y, @z))
+  end
+
+  def test_e
+    assert_equal(false, func_e(@a, @b, @c))
+  end
+
+  def test_f
+    assert_equal(true, func_f(@x, @y, @z))
+  end
+
+  def test_g
+    assert_equal(false, func_g(@a, @b, @c))
+  end
+
+end
+
+class UnitTest2 < MiniTest::Unit::TestCase
+  def setup
+    @a = 2.5
+    @p = true
+  end
+  def test
+    assert_equal(false, func_second(@a, @p))
+  end
+end
--- Lab3/lab3_2.rb
+++ Lab3/lab3_2.rb
@@ -1,28 +1,70 @@
-# input
-print "x ="
-x = gets.chomp.to_f
+require 'minitest/autorun'
 
-# if
-y = if x > -4 && x <= 0
-      (((x - 2).abs) / (x**2 * Math.cos(x))) ** (1 .fdiv 7)
-    elsif x > 0 && x <= 12
-      1 / (Math.tan(x + 1/Math.exp(x)) / Math.sin(x)**2) ** (2 .fdiv 7)
-    elsif x < -4 || x > 12
-      1 / (1 + x / (1 + x / (1 + x)))
-    end
+def y_if(x)
+  if x > -4 && x <= 0
+    (((x - 2).abs) / (x**2 * Math.cos(x))) ** (1 .fdiv 7)
+  elsif x > 0 && x <= 12
+    1 / (Math.tan(x + 1/Math.exp(x)) / Math.sin(x)**2) ** (2 .fdiv 7)
+  elsif x < -4 || x > 12
+    1 / (1 + x / (1 + x / (1 + x)))
+  end
+end
 
-puts "y = #{y}"
-# case
+def y_case(x)
+  case x
+  when (-4.0).next_float..0
+    (((x - 2).abs) / (x**2 * Math.cos(x))) ** (1 .fdiv 7)
+  when (0.0).next_float..12
+    1 / (Math.tan(x + 1/Math.exp(x)) / Math.sin(x)**2) ** (2 .fdiv 7)
+  when -Float::INFINITY...-4
+    1 / (1 + x / (1 + x / (1 + x)))
+  when (12..)
+    1 / (1 + x / (1 + x / (1 + x)))
+  end
+end
 
-y = case x
-    when (-4.0).next_float..0
-      (((x - 2).abs) / (x**2 * Math.cos(x))) ** (1 .fdiv 7)
-    when (0.0).next_float..12
-      1 / (Math.tan(x + 1/Math.exp(x)) / Math.sin(x)**2) ** (2 .fdiv 7)
-    when -Float::INFINITY...-4
-      1 / (1 + x / (1 + x / (1 + x)))
-    when (12..)
-      1 / (1 + x / (1 + x / (1 + x)))
-    end
+class UnitTestIf < MiniTest::Unit::TestCase
+  def setup
+    #do nothing
+  end
 
-puts "y = #{y}"
+  def test_1
+    assert_in_delta(1.277, y_if(-1), 1e-3)
+  end
+
+  def test_2
+    assert_in_delta 0.576, y_if(1.0), 1e-3
+  end
+
+  def test_3
+    assert_in_delta(-0.818, y_if(-5.0), 1e-3)
+  end
+
+  def test_4
+    assert_in_delta(0.129, y_if(13.0), 1e-3)
+  end
+
+end
+
+class UnitTestCase < MiniTest::Unit::TestCase
+  def setup
+    #do nothing
+  end
+
+  def test_1
+    assert_in_delta(1.277, y_case(-1), 1e-3)
+  end
+
+  def test_2
+    assert_in_delta 0.576, y_case(1.0), 1e-3
+  end
+
+  def test_3
+    assert_in_delta(-0.818, y_case(-5.0), 1e-3)
+  end
+
+  def test_4
+    assert_in_delta(0.129, y_case(13.0), 1e-3)
+  end
+
+end
